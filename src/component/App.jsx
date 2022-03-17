0ea2dbf2b2fcf328cdeefb886ee99bc5ec78c4ea
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-03-17 14:11:46 +0330

    project creation

---
+++ src/component/App.jsx
@@ -0,0 +1,13 @@
+import '../asset/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Ticket Reservation
+      </h1>
+    </div>
+  );
+}
+
+export default App;
