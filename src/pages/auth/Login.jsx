6a5afcd86663586e9c60762449f94078c3475940
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-24 18:12:19 +0430

    Add some function and state to use theme for send API request

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Login() {
 	//Create array for  titles
 	const titles = [
@@ -8,15 +10,32 @@ function Login() {
 		"حساب کاربری ندارید؟",
 	];
 
+	//Create this state to control inputs and use them to send API requests
+	const [user, setUser] = useState({
+		username: "",
+		password: "",
+	});
+
+	//Create this function to register and send API requests
+	const login = () => {};
+
 	//Create Login component
 	return (
-		<form className="sign-up">
+		<form className="sign-up" onSubmit={login}>
 			<div className="sign-header">
 				<h1>{titles[0]}</h1>
 				<h4>{titles[1]}</h4>
 			</div>
-			<input type="text" placeholder="نام کاربری" />
-			<input type="password" placeholder="رمز ورود" />
+			<input
+				type="text"
+				placeholder="نام کاربری"
+				onChange={(e) => setUser({ ...user, username: e.target.value })}
+			/>
+			<input
+				type="password"
+				placeholder="رمز ورود"
+				onChange={(e) => setUser({ ...user, password: e.target.value })}
+			/>
 			<button type="button">{titles[3]}</button>
 			<div class="sign-footer">
 				<p>{titles[2]}</p>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Register() {
 	//Create array for  titles
 	const titles = [
@@ -7,21 +9,71 @@ function Register() {
 		"حساب کاربری دارید؟",
 	];
 
+	//Create this state to control inputs and use them to send API requests
+	const [user, setUser] = useState({
+		fullName: "",
+		username: "",
+		password: "",
+		phoneNumber: "",
+		email: "",
+		nationalCode: "",
+		birthDate: "",
+	});
+
+	//Create this function to register and send API requests
+	const register = () => {};
+
 	//Create Register component
 	return (
-		<form className="sign-in">
+		<form className="sign-in" onSubmit={register}>
 			<div className="sign-header">
 				<h1>{titles[0]}</h1>
 				<h4>{titles[1]}</h4>
 			</div>
-			<input type="text" placeholder="نام و نام خانوادگی" />
-			<input type="text" placeholder="نام کاربری" />
-			<input type="password" placeholder="رمز ورود" />
+			<input
+				type="text"
+				placeholder="نام و نام خانوادگی"
+				onChange={(e) =>
+					setUser({ ...user, fullNameu: e.target.value })
+				}
+			/>
+			<input
+				type="text"
+				placeholder="نام کاربری"
+				onChange={(e) => setUser({ ...user, username: e.target.value })}
+			/>
+			<input
+				type="password"
+				placeholder="رمز ورود"
+				onChange={(e) => setUser({ ...user, password: e.target.value })}
+			/>
 			<input type="password" placeholder="تکرار رمز ورود" />
-			<input type="tell" placeholder="شماره همراه" />
-			<input type="email" placeholder="ایمیل" />
-			<input type="number" placeholder="کد ملی" />
-			<input type="date" placeholder="تاریخ تولد" />
+			<input
+				type="tell"
+				placeholder="شماره همراه"
+				onChange={(e) =>
+					setUser({ ...user, phoneNumber: e.target.value })
+				}
+			/>
+			<input
+				type="email"
+				placeholder="ایمیل"
+				onChange={(e) => setUser({ ...user, email: e.target.value })}
+			/>
+			<input
+				type="number"
+				placeholder="کد ملی"
+				onChange={(e) =>
+					setUser({ ...user, nationalCode: e.target.value })
+				}
+			/>
+			<input
+				type="date"
+				placeholder="تاریخ تولد"
+				onChange={(e) =>
+					setUser({ ...user, birthDate: e.target.value })
+				}
+			/>
 			<button type="button">{titles[2]}</button>
 			<div className="sign-footer">
 				<p>{titles[3]}</p>
