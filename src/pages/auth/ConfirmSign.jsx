5e586c2e50bab3bb909dab20d07bb42929612238
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-25 18:41:49 +0430

    Create confirmation of sign page

---
+++ src/pages/auth/ConfirmSign.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+function ConfirmSign() {
+	//Create array for  titles
+	const titles = [
+		"به تراکت خوش آمدید",
+		".لطفا برای ادامه کد تایید را واردکنید",
+		"ثبت نام / ورود",
+	];
+
+	//Create this state to control inputs and use them to send API requests
+	const [confirmCode, setConfirmCode] = useState({
+		confirmCode: "",
+	});
+
+	//Create this function to confirm sign in or sign up and send API requests
+	const confirmation = () => {};
+
+	//Create confirmation component
+	return (
+		<div className="sign-up">
+			<div className="sign-header">
+				<h1>{titles[0]}</h1>
+				<h4>{titles[1]}</h4>
+			</div>
+			<form className="sign-footer" onSubmit={confirmation}>
+				<input
+					type="number"
+					placeholder="کد تایید"
+					onChange={(e) =>
+						setConfirmCode({ ...confirmCode, confirmCode: e.target.value })
+					}
+				/>
+				<button type="button">{titles[2]}</button>
+			</form>
+		</div>
+	);
+}
+
+export default ConfirmSign;
