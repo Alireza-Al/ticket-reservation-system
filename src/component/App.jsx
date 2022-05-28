bd28003a546059acf62c0194bbdc317593340452
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-28 17:57:09 +0430

    Add router to project and set pages

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,10 +1,15 @@
 import '../asset/App.scss';
 import Home from '../pages/homePage/Home';
+import Sign from '../pages/auth/Sign';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/users/login" element={<Sign />} />
+      </Routes>
     </div>
   );
 }
