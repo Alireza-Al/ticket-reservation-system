2c0e4ca4823516d15b0bf4fc4af054c5e64703b7
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-29 21:10:06 +0430

    dahsboard menu onclick color change states handled

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,34 +1,79 @@
 import React, { useState } from 'react'
 
 function Dashboard() {
-  return (
-    <div className='dashboard-container'>
-        <div className="dashboard-self">
-            <div className="dashboard-menu"> {/*The menu with orange color on the right of dashboard*/}
-                <div className="dashboard-item">
-                    <span>
-                        داشبورد
-                    </span>
-                </div>
-                <div className="dashboard-item">
-                    <span>
-                        اطلاعات کاربری
-                    </span>
-                </div>
-                <div className="dashboard-item">
-                    <span>
-                        کیف پول
-                    </span>
-                </div>
-                <div className="dashboard-item">
-                    <span>
-                        تاریخچه خرید
-                    </span>
+
+    const [active, setActive] = useState({
+        dahsboard: false,
+        userData: false,
+        pocket: false,
+        history: false
+    })
+
+    return (
+        <div className='dashboard-container'>
+            <div className="dashboard-self">
+                <div className="dashboard-menu"> {/*The menu with orange color on the right of dashboard*/}
+                    <div className="dashboard-item"
+                        onClick={() => {
+                            setActive({
+                                dahsboard: true,
+                                userData: false,
+                                pocket: false,
+                                history: false
+                            })
+                        }}
+                        style={{ backgroundColor: active.dahsboard ? '#FF4E00' : 'initial', borderRadius: '0px 44px 0px 0px' }}>
+                        <span>
+                            داشبورد
+                        </span>
+                    </div>
+                    <div className="dashboard-item"
+                        onClick={() => {
+                            setActive({
+                                dahsboard: false,
+                                userData: true,
+                                pocket: false,
+                                history: false
+                            })
+                        }}
+                        style={{ backgroundColor: active.userData ? '#FF4E00' : 'initial' }}>
+                        <span>
+                            اطلاعات کاربری
+                        </span>
+                    </div>
+                    <div className="dashboard-item" onClick={() => {
+                        setActive({
+                            dahsboard: false,
+                            userData: false,
+                            pocket: true,
+                            history: false
+                        })
+                    }}
+                        style={{ backgroundColor: active.pocket ? '#FF4E00' : 'initial' }}>
+
+                        <span>
+                            کیف پول
+                        </span>
+                    </div>
+                    <div className="dashboard-item"
+                        onClick={() => {
+                            setActive({
+                                dahsboard: false,
+                                userData: false,
+                                pocket: false,
+                                history: true
+                            })
+                        }}
+                        style={{ backgroundColor: active.history ? '#FF4E00' : 'initial' }}>
+
+                        <span>
+                            تاریخچه خرید
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Dashboard
