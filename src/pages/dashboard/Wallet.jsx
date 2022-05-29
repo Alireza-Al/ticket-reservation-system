222babea645afd076057a7fc6d00148707ecf9a5
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-30 00:26:02 +0430

    wallet static made, input onChange managment done

--- src/pages/dashboard/Wallet.jsx
+++ src/pages/dashboard/Wallet.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Wallet() {
-  return (
-    <div>Wallet</div>
-  )
+
+    const [walletNumber, setWalletNumber] = useState(0)
+
+    return (
+        <div>
+            <h3>
+                برای شارژ کردن کیف پول، ابتدا مبلغ را وارد کنید، سپس دکمه ”شارژ” را بزنید.
+            </h3>
+            <div className="price-input">
+                <label htmlFor="">
+                    مبلغ :
+                    <input type="number" value={walletNumber} onChange={(e) => {
+                        let value = walletNumber
+                        value = e.target.value
+                        setWalletNumber(value)
+                    }} />
+                </label>
+                <button className="submit-wallet-change">
+                    شارژ
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Wallet
