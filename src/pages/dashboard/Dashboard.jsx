a64c36e7f2ed3f6f5f33709ab3ea92b8ce0a905a
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-29 22:23:12 +0430

    dashboard and dashboard menu bugs fixed

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,27 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import DashboardMenu from './dashboardMenu/DashboardMenu'
 
 function Dashboard() {
 
     const [active, setActive] = useState({
-        dahsboard: false,
         userData: false,
         pocket: false,
         history: false
     })
 
-    const changeActivation = (detail) => {
+    const changeActivation = (state) => {
         setActive({
-            dashboard : detail.dashboard,
-            userData : detail.userData,
-            pocket : detail.pocket,
-            history : detail.history,
+            userData : state.userData,
+            pocket : state.pocket,
+            history : state.history,
         })
     }
-    
-    useEffect(() => {
-        console.log(active);
-    }, [active])
 
     return (
         <div className='dashboard-container'> {/* the container of the dashboard page */}
--- src/pages/dashboard/dashboardMenu/DashboardMenu.jsx
+++ src/pages/dashboard/dashboardMenu/DashboardMenu.jsx
@@ -11,54 +11,41 @@ function DashboardMenu(props) {
 
     return (
         <div className="dashboard-menu"> {/*The menu with orange color on the right of dashboard*/}
-            <div className="dashboard-item"
+            <div
+                className="dashboard-item"
                 onClick={() => {
                     setState({
-                        dashboard: true,
-                        userData: false,
-                        pocket: false,
-                        history: false
-                    })
-                }}
-                style={{ backgroundColor: state.dashboard ? '#FF4E00' : 'initial', borderRadius: '0px 36px 0px 0px' }}>
-                {/* whenever each state is true the color will change by the click */}
-                <span>
-                    داشبورد
-                </span>
-            </div>
-            <div className="dashboard-item"
-                onClick={() => {
-                    setState({
-                        dahsboard: false,
                         userData: true,
                         pocket: false,
                         history: false
                     })
                 }}
-                style={{ backgroundColor: state.userData ? '#FF4E00' : 'initial' }}>
+                style={{ backgroundColor: state.userData ? '#FF4E00' : 'initial', borderRadius: '0px 36px 0px 0px' }}>
                 {/* whenever each state is true the color will change by the click */}
                 <span>
                     اطلاعات کاربری
                 </span>
             </div>
-            <div className="dashboard-item" onClick={() => {
-                setState({
-                    dahsboard: false,
-                    userData: false,
-                    pocket: true,
-                    history: false
-                })
-            }}
+            <div
+                className="dashboard-item"
+                onClick={() => {
+                    setState({
+                        dahsboard: false,
+                        userData: false,
+                        pocket: true,
+                        history: false
+                    })
+                }}
                 style={{ backgroundColor: state.pocket ? '#FF4E00' : 'initial' }}>
                 {/* whenever each state is true the color will change by the click */}
                 <span>
                     کیف پول
                 </span>
             </div>
-            <div className="dashboard-item"
+            <div
+                className="dashboard-item"
                 onClick={() => {
                     setState({
-                        dahsboard: false,
                         userData: false,
                         pocket: false,
                         history: true
