be5c94d1f86e3277ed133b010f2cef367afdc28a
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-29 23:52:18 +0430

    user data static made

--- src/pages/dashboard/UserData.jsx
+++ src/pages/dashboard/UserData.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function UserData() {
-  return (
-    <div>UserData</div>
-  )
+
+    const containerNames = [
+        'نام :',
+        'نام خانوادگی :',
+        'کد ملی :',
+        'ایمیل :',
+        'تاریخ تولد :'
+    ]
+
+
+    return (
+        <div> {/* cont means container */}
+            {
+                containerNames.map((item, index) => {
+                    return <Fragment key={index}>
+                        <div className="item-cont">
+                            <span className="cont-name">
+                                {item}
+                            </span>
+                            <span className="cont-value">
+                                علیرضا الهوردی
+                            </span>
+                        </div>
+                    </Fragment>
+                })
+            }
+            <button className='change-info'>
+                تغییر اطلاعات کاربری
+            </button>
+        </div>
+    )
 }
 
 export default UserData
