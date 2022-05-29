bd7ca52eed41ba82c256312af1af760bb56d21e5
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-29 23:04:53 +0430

    dashboard items general stae made and imported

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,26 +1,36 @@
 import React, { useState } from 'react'
 import DashboardMenu from './dashboardMenu/DashboardMenu'
+import PurchaseHistory from './PurchaseHistory'
+import UserData from './UserData'
+import Wallet from './Wallet'
 
 function Dashboard() {
 
-    const [active, setActive] = useState({
-        userData: false,
-        pocket: false,
-        history: false
-    })
+    const [active, setActive] = useState({})
 
     const changeActivation = (state) => {
         setActive({
-            userData : state.userData,
-            pocket : state.pocket,
-            history : state.history,
+            userData: state.userData,
+            wallet: state.wallet,
+            history: state.history,
         })
     }
 
     return (
         <div className='dashboard-container'> {/* the container of the dashboard page */}
             <div className="dashboard-self"> {/* the container of the dashboard box*/}
-                <DashboardMenu active={changeActivation}/>
+                <DashboardMenu active={changeActivation} />
+                <div className="item-data">
+                    {
+                        active.userData ?
+                            <UserData />
+                            : active.wallet ?
+                                <Wallet />
+                                : active.history ?
+                                    <PurchaseHistory />
+                                    : ''
+                    }
+                </div>
             </div>
         </div>
     )
