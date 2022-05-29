1ffa3df64ca829a19e11bdc81af4de437fb430f1
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-29 20:59:24 +0430

    dashboard menu onthe right created

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dashboard() {
   return (
     <div className='dashboard-container'>
         <div className="dashboard-self">
-            
+            <div className="dashboard-menu"> {/*The menu with orange color on the right of dashboard*/}
+                <div className="dashboard-item">
+                    <span>
+                        داشبورد
+                    </span>
+                </div>
+                <div className="dashboard-item">
+                    <span>
+                        اطلاعات کاربری
+                    </span>
+                </div>
+                <div className="dashboard-item">
+                    <span>
+                        کیف پول
+                    </span>
+                </div>
+                <div className="dashboard-item">
+                    <span>
+                        تاریخچه خرید
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
   )
