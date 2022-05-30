9c1aa9214037cb57104538faaff53ffa6702d77a
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-30 20:37:37 +0430

    Add ticket reducer to reducers

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
-import userReducer from "./userReducer"
+import userReducer from "./userReducer";
+import ticketReducer from "./ticketReducer";
 
 const rootReducer = combineReducers({
-    user: userReducer
+    user: userReducer,
+    ticket: ticketReducer,
 });
 
 export default rootReducer;
