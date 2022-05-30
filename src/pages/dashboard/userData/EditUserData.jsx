ef11178772b8c9e8dd37e1a97506e085f1683299
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-05-31 00:51:46 +0430

    edit user api put handled

--- src/pages/dashboard/userData/EditUserData.jsx
+++ src/pages/dashboard/userData/EditUserData.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from '../../../api/axios';
 
 function EditUserData(props) {
 
@@ -6,6 +7,24 @@ function EditUserData(props) {
 
     const HandleSubmit = (e) => {
         e.preventDefault();
+        const token = `Token ${JSON.parse(localStorage.getItem("token"))}`;
+
+		const config = {
+			headers: {
+				Authorization: token,
+			},
+		};
+        axios
+            .put(`/customer/${props.id}/`, {
+                first_name: info.name,
+                last_name: info.lastName,
+                national_code: info.idNum,
+                birth_date: info.birthDate
+            },config)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.error(err))
     }
 
     return (
@@ -44,6 +63,30 @@ function EditUserData(props) {
                             setInfo({
                                 ...info,
                                 idNum: e.target.value
+                            })
+                        }} />
+                </label>
+                <label htmlFor="">
+                    {props.containerNames[3]}
+                    <input
+                        type="email"
+                        // value={props.initValue.idNum}
+                        onChange={(e) => {
+                            setInfo({
+                                ...info,
+                                email: e.target.value
+                            })
+                        }} />
+                </label>
+                <label htmlFor="">
+                    {props.containerNames[4]}
+                    <input
+                        type="date"
+                        // value={props.initValue.idNum}
+                        onChange={(e) => {
+                            setInfo({
+                                ...info,
+                                birthDate: e.target.value
                             })
                         }} />
                 </label>
