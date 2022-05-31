f8133d0956dcf460ed48099ba710b20bffd7bf68
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-05-31 10:32:06 +0430

    Show wallet credit

--- src/pages/dashboard/Wallet.jsx
+++ src/pages/dashboard/Wallet.jsx
@@ -5,6 +5,29 @@ function Wallet() {
 	const [walletNumber, setWalletNumber] = useState(0);
 
 	let walletId = null;
+	const [credit, setCredit] = useState(0);
+
+	useEffect(() => {
+
+		const token = `Token ${JSON.parse(localStorage.getItem("token"))}`;
+
+		const config = {
+			headers: {
+				Authorization: token,
+			},
+		};
+
+		axios
+			.get("/user/wallet/", config)
+			.then((response) => {
+				walletId = response.data.id;
+				setCredit(response.data.amount);
+			}, [])
+			.catch((err) => {
+				console.error(err);
+			});
+
+	})
 
 	const charge = () => {
 		const token = `Token ${JSON.parse(localStorage.getItem("token"))}`;
@@ -18,7 +41,6 @@ function Wallet() {
 			.get("/user/wallet/", config)
 			.then((response) => {
 				walletId = response.data.id;
-
 				axios
 					.patch(
 						`/wallet/${walletId}`,
@@ -43,6 +65,17 @@ function Wallet() {
 				برای شارژ کردن کیف پول، ابتدا مبلغ را وارد کنید، سپس دکمه ”شارژ”
 				را بزنید.
 			</h3>
+			<div className="price-input">
+				<p>موجودی کیف پول:</p>
+				{
+					credit !==0 ?
+					(
+						 <p>{credit}</p>
+					):(
+						<h3>Loading...</h3>
+					)
+				}
+			</div>
 			<div className="price-input">
 				<label htmlFor="">
 					مبلغ :
