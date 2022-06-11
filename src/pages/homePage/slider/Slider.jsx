88ab431dac333838162e62b7ad6269149b4b8332
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-06-11 12:50:55 +0430

    created the base slider to be replaced by images

--- src/pages/homePage/slider/Slider.jsx
+++ src/pages/homePage/slider/Slider.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "swiper/scss/navigation";
+
+import { Autoplay, Pagination, Navigation } from "swiper";
+
 
 function Slider() {
-  return (
-    <div className='slider'></div>
-  )
+    return (
+        <div className='slider'>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>1</SwiperSlide>
+                <SwiperSlide>2</SwiperSlide>
+                <SwiperSlide>3</SwiperSlide>
+                <SwiperSlide>4</SwiperSlide>
+                <SwiperSlide>5</SwiperSlide>
+                <SwiperSlide>6</SwiperSlide>
+                <SwiperSlide>7</SwiperSlide>
+                <SwiperSlide>8</SwiperSlide>
+            </Swiper>
+        </div>
+    )
 }
 
 export default Slider
