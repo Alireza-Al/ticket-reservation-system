369445471c73bf3cb4b9c8b33a5d8fb2c69cd87b
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-05 23:31:09 +0430

    home page services part made responsive

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -7,7 +7,7 @@ import { faTwitter, faInstagram, faLinkedin, faTelegram } from '@fortawesome/fre
 import { faCircleDollarToSlot, faComment, faEarthAsia, faHeadphonesSimple } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
-	
+
 	return (
 		<div className="home">
 			<SearchBox />
@@ -30,7 +30,7 @@ function Home() {
 							تضمین بهترین قیمت
 						</p>
 						<p className="desc">
-							بیشترین تخفیف ها برای رزرو هتل و خرید بلیط
+							رزرو هتل و خربد بلیط سریع و راحت
 						</p>
 					</div>
 				</div>
