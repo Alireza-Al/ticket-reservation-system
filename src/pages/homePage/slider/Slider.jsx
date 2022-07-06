ef529a67363761841f8dc60e136434b410f6955a
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-06 18:59:47 +0430

    slider made ultra responsive

--- src/pages/homePage/slider/Slider.jsx
+++ src/pages/homePage/slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/scss";
@@ -9,9 +9,34 @@ import { Autoplay, Pagination, Navigation } from "swiper";
 
 
 function Slider() {
+
+    const [swiperSlides, setSwiperSlides] = useState(3);
+    
+
+    useEffect(() => {
+        // this function will change the number of slides to be shown after a certain number of innerWidth
+        window.addEventListener("resize",() => {
+            if (window.innerWidth < '900') {
+                setSwiperSlides(2)
+            }
+            if (window.innerWidth < '650') {
+                setSwiperSlides(1)
+            }
+            else {
+                setSwiperSlides(3)
+            }
+        })
+    },[])
+
+    useEffect(() => {
+        console.log(swiperSlides);
+    },[swiperSlides])
+
     return (
         <div className='slider'>
+            <img className='imgLarge' src="https://cdnsnapptrip.com/images/market/Uploads/banners/website/hotel/large/20220625/1-mobile-20220625.jpg" alt="" />
             <Swiper
+                slidesPerView={swiperSlides}
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
@@ -27,14 +52,11 @@ function Slider() {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src="https://cdnsnapptrip.com/images/market/Uploads/banners/website/hotel/large/20220625/1-mobile-20220625.jpg" alt="" />
-                    </SwiperSlide>
-                <SwiperSlide>
                     <img src="https://cdnsnapptrip.com/images/market/Uploads/banners/website/hotel/medium/20220625/1-desktop-20220625.jpg" alt="" />
-                    </SwiperSlide>
+                </SwiperSlide>
                 <SwiperSlide>
                     <img src="https://cdnsnapptrip.com/images/market/Uploads/banners/website/hotel/medium/20220625/2-desktop-20220625.jpg" alt="" />
-                    </SwiperSlide>
+                </SwiperSlide>
                 <SwiperSlide>
                     <img src="https://cdnsnapptrip.com/images/market/Uploads/banners/website/hotel/medium/20220625/4-desktop-20220625.jpg" alt="" />
                 </SwiperSlide>
