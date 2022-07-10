30b7c14480c3f02eb50f3b34c454dd22e01efbbb
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-07-10 15:05:54 +0430

    dashboard menu made responsive

--- src/pages/dashboard/dashboardMenu/DashboardMenu.jsx
+++ src/pages/dashboard/dashboardMenu/DashboardMenu.jsx
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHistory, faUser, faWallet } from '@fortawesome/free-solid-svg-icons'
 
 function DashboardMenu(props) {
 
     const [state, setState] = useState({
         userData: true
     })
+
+    const [small, setSmall] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            let width = window.innerWidth
+            if (width <= "700") {
+                setSmall(true)
+            }
+            else {
+                setSmall(false)
+            }
+        })
+    }, [])
 
     useEffect(() => {
         props.active(state)
@@ -23,9 +39,16 @@ function DashboardMenu(props) {
                 }}
                 style={{ backgroundColor: state.userData ? '#FF4E00' : 'initial', borderRadius: '0px 36px 0px 0px' }}>
                 {/* whenever each state is true the color will change by the click */}
-                <span>
-                    اطلاعات کاربری
-                </span>
+                {
+                    small ?
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faUser} />
+                        </span>
+                        :
+                        <span className='text'>
+                            اطلاعات کاربری
+                        </span>
+                }
             </div>
             <div
                 className="dashboard-item"
@@ -38,9 +61,16 @@ function DashboardMenu(props) {
                 }}
                 style={{ backgroundColor: state.wallet ? '#FF4E00' : 'initial' }}>
                 {/* whenever each state is true the color will change by the click */}
-                <span>
-                    کیف پول
-                </span>
+                {
+                    small ?
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faWallet} />
+                        </span>
+                        :
+                        <span className='text'>
+                            کیف پول
+                        </span>
+                }
             </div>
             <div
                 className="dashboard-item"
@@ -53,9 +83,16 @@ function DashboardMenu(props) {
                 }}
                 style={{ backgroundColor: state.history ? '#FF4E00' : 'initial' }}>
                 {/* whenever each state is true the color will change by the click */}
-                <span>
-                    تاریخچه خرید
-                </span>
+                {
+                    small ?
+                        <span className='icon'>
+                            <FontAwesomeIcon icon={faHistory} />
+                        </span>
+                        :
+                        <span className='text'>
+                            تاریخچه خرید
+                        </span>
+                }
             </div>
         </div>
     )
